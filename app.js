62debb379f3b4ec56efeddba7d31f4ba19f7243b
tvpeter <withtvpeter@gmail.com>
2018-11-16 02:55:28 +0100

    add routes separation and views rendering

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
+
 const app = express();
-const port = process.env.PORT || 3000;
-const booksRouter = express.Router();
+const port = process.env.PORT || 8080;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public/')));
@@ -16,16 +16,16 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist/'))
 app.set('views', './src/views/');
 app.set('view engine', 'ejs');
 
+const nav = [{ link: '/books', title: 'Book' },
+  { link: '/authors', title: 'Author' }];
+const bookRouter = require('./src/routes/bookRoutes')(nav);
 
-app.use('/books', booksRouter);
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.render('index',
-    {
-      nav: [{ link: '/books', title: 'Books' },
-        { link: '/authors', title: 'Authors' }
-      ],
-      title: 'Libary'
-    });
+  res.render('index', {
+    nav,
+    title: 'Library'
+  });
 });
 
 app.listen(port, () => {
